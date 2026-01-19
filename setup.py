0d3c54f050486602e07aa832dca253beec363d75
Mekonnen Abyot Melkamu <89458721+abyot2021@users.noreply.github.com>
2026-01-19 10:45:39 +0900

    Create setup.py for SoloMicrobe package
    Initial setup.py for SoloMicrobe package with metadata.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import io
+import setuptools
+
+
+with io.open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SoloMicrobe",
+    version="0.1.0",
+    author="Abyot Melkamu",
+    author_email="abyot@pusan.ac.kr",
+    description="SoloMicrobe",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pnucolab/probe-designer-rtd-tutorial",
+    packages=setuptools.find_packages(),
+)
